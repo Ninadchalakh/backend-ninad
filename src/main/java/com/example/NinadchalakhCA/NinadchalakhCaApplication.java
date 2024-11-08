b694722c25d05bc48bf96bbb3c45b5359a0e9504
Ninadchalakh <ninadchalakh@gmail.com>
2024-11-08 13:41:51 +0530

    first commit

---
+++ src/main/java/com/example/NinadchalakhCA/NinadchalakhCaApplication.java
@@ -0,0 +1,13 @@
+package com.example.NinadchalakhCA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinadchalakhCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinadchalakhCaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NinadchalakhCA/NinadchalakhCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NinadchalakhCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NinadchalakhCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
